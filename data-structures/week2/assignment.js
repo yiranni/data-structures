994037f4ebeeeb09a1573cff7ffb1b879cd56c22
yiranni <yirann@uw.edu>
2019-10-02 23:58:01 +0000

    initiate final project 1

--- data-structures/week2/assignment.js
+++ data-structures/week2/assignment.js
@@ -15,22 +15,31 @@ var $ = cheerio.load(content);
 // write the addresses to a text file
 var meetingData = [];
 
+
 // find 'td' tag
 $('td').each(function(i, elem) {
     if($(elem).attr('style') === "border-bottom:1px solid #e3e3e3; width:260px") {
-        // m05meetingAddress += ($(elem).text()).trim() + '\n' + '\n';
-        // console.log($(elem).html().split('<br>')[2].trim().split(',')[0]);
         var thisMeeting = {};
         thisMeeting.streetAddress = $(elem).html().split('<br>')[2].trim().split(',')[0];
         thisMeeting.city = "New York";
         thisMeeting.state = "NY";
         thisMeeting.zip =   $(elem).html().split('<br>')[3].trim().slice(-5);
-        console.log(thisMeeting.zip);
+        var details = $($(elem).html().split('<br>')[5].trim().split('<span')[0]).text().trim();
+        thisMeeting.details = details;
+        // console.log(details)
+        var accessibility;
+        if($(elem).html().includes('wheelchair')) {
+            accessibility = true;
+        }else {
+            accessibility = false
+        }
+        thisMeeting.accessibility = accessibility;
         meetingData.push(thisMeeting);
+       
     }
 });
 
-// console.log(meetingData[0].streetAddress);
+console.log(meetingData);
 // console.log(meetingData.length);
 
 
--- data-structures/week3/writeFile.js
+++ data-structures/week3/writeFile.js
@@ -14,6 +14,17 @@ for (var i = 0; i < dataWithZip.length; i++) {
     zipArry.push(dataWithZip[i].zip);
 }
 
+var detailsArry = [];
+for (var i = 0; i < dataWithZip.length; i++) {
+    detailsArry.push(dataWithZip[i].details);
+}
+
+var accessibilityArry = [];
+for (var i = 0; i < dataWithZip.length; i++) {
+    accessibilityArry.push(dataWithZip[i].accessibility);
+}
+// console.log(dataWithZip)
+
 //  array with street, city, state, zipcode, lat, long
 // read data from both json and text
 var addressArry = [];
@@ -26,12 +37,15 @@ for(var i = 0; i < data.length; i++) {
     thisAddress.city = cityInfo;
     thisAddress.state = stateInfo;
     thisAddress.zip = zipArry[i];
+    
     thisAddress.latitude = data[i].OutputGeocodes[0].OutputGeocode.Latitude;
     thisAddress.longitude = data[i].OutputGeocodes[0].OutputGeocode.Longitude;
+    thisAddress.details = detailsArry[i];
+    thisAddress.accessibility = accessibilityArry[i];
     addressArry.push(thisAddress);
     
 }
 
-
+console.log(addressArry)
 // write data(array) to a text file
-fs.writeFileSync('/home/ec2-user/environment/data-structures/data/m05meetingAddress-latlong.json', JSON.stringify(addressArry));
+fs.writeFileSync('/home/ec2-user/environment/data-structures/data/m05meetingAddress.json', JSON.stringify(addressArry));

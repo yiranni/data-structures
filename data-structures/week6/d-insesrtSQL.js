b76580534d1ff5e78fb49802496cce744b3eaf71
yiranni <yirann@uw.edu>
2019-10-12 21:03:47 +0000

    hide psw

--- data-structures/week6/d-insesrtSQL.js
+++ data-structures/week6/d-insesrtSQL.js
@@ -6,7 +6,7 @@ var db_credentials = new Object();
 db_credentials.user = 'yiranni';
 db_credentials.host = 'aameeting.cowtwtgreshm.us-east-1.rds.amazonaws.com';
 db_credentials.database = 'aa';
-db_credentials.password = 'Niyiran971022!';
+db_credentials.password = process.env.AWSRDS_PW;;
 db_credentials.port = 5432;
 
 // Connect to the AWS RDS Postgres database
